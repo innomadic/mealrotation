b6572ce71ab01abaa7a0a5ba9dc858fe8efab134
Nick <nick@innomadic.com>
2022-12-03 11:02:33 +0300

    added dinner

--- meals.py
+++ meals.py
@@ -1,19 +1,22 @@
 '''
 Prepares a 52 week meal plan, rotating through lists of meals for each day.
+
+`python3 meals.py > plan.md`
 '''
 meals = {
     "Saturday": {
-        "breakfast": ["Baked oatmeal"],
-        "other": [
+        "breakfast": ["Baked Oatmeal"],
+        "lunch": [
             "Spaghetti",
             "Grilled Chicken with Vegetables",
             "Meatloaf",
             "Biscuits and Gravy with eggs",
         ],
+        'dinner': ['Leftovers']
     },
     "Sunday": {
         "breakfast": ["Waffles with Eggs", "Pancakes with Eggs"],
-        "other": [
+        "lunch": [
             "Butter Chicken",
             "Potato Curry",
             "Chili",
@@ -22,46 +25,53 @@ meals = {
             "Lentils",
             "Lasagna",
         ],
+        'dinner': ['Leftovers']
     },
     "Monday": {
         "breakfast": ["Crepes with Shakshuka/Eggs"],
-        "other": [
+        "lunch": [
             "Fried Chicken with Fries",
             "Korean Fried Chicken",
             "Chicken Parmesean",
         ],
+        'dinner': ['Leftovers']
     },
     "Tuesday": {
         "breakfast": ["Toast with Jelly and Eggs", "Cinnamon Toast with Eggs"],
-        "other": ["Tacos", "Enchiladas", "Quesadillas"],
+        "lunch": ["Tacos", "Enchiladas", "Quesadillas"],
+        'dinner': ['Leftovers'],
     },
     "Wednesday": {
         "breakfast": ["Hash browns with Eggs"],
-        "other": [
+        "lunch": [
             "Steak with Asian Slaw",
             "Steak with Vegetables and Rolls",
             "Steak with Baked Potatoes/Sweet Potatoes/Mashed Potatoes",
         ],
+        'dinner': ['Leftovers']
     },
     "Thursday": {
         "breakfast": ["French Toast", "Biscuits with Gravy and Eggs"],
-        "other": ["Pizza", "Grilled Cheese", "Burgers with Fries", "Restaurant"],
+        "lunch": ["Pizza", "Grilled Cheese", "Burgers with Fries", "Restaurant"],
+        'dinner': ['Leftovers']
     },
     "Friday": {
         "breakfast": ["Cinnamon Rolls", "Granola with Fruit and Yogurt"],
-        "other": [
+        "lunch": [
             "Restaurant",
             "Popcorn, Apples, Snacks",
             "Pita Chips, Vegetables, Hummus, Feta",
         ],
+        'dinner': ['Leftovers']
     },
 }
 
 for i in range(1, 53):
-    print(f"{i}")
+    print(f"# Week {i}")
     for day, meal_choices in meals.items():
-        print(f"    {day}")
+        print(f"## {day}")
         print(
-            f'      Breakfast: {meal_choices["breakfast"][i%len(meal_choices["breakfast"])]}'
+            f'* Breakfast: {meal_choices["breakfast"][i%len(meal_choices["breakfast"])]}'
         )
-        print(f'      Lunch: {meal_choices["other"][i%len(meal_choices["other"])]}')
+        print(f'* Lunch: {meal_choices["lunch"][i%len(meal_choices["lunch"])]}')
+        print(f'* Dinner: {meal_choices["dinner"][i%len(meal_choices["dinner"])]}')
